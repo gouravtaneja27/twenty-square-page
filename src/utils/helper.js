4dec08ac6d6753a674689a54e27175f76ffbde8e
Gourav Taneja <gouravtaneja@Gouravs-MacBook-Air.local>
2026-03-29 15:59:23 +0530

    updated

---
+++ src/utils/helper.js
@@ -0,0 +1,30 @@
+export const navLInks = [
+    {
+        title: "Home",
+        url: "/",
+    },
+    {
+        title: "Offers",
+        url: "/offers",
+    },
+    {
+        title: "Gift Card",
+        url: "/gift-card",
+    },
+    {
+        title: "Contact Us",
+        url: "/contact-us"
+    }
+];
+
+export const sliderDAta = [
+    {
+        src: "/assets/slider-img.jpg",
+    },
+    {
+        src: "/assets/slider-img.jpg",
+    },
+    {
+        src: "/assets/slider-img.jpg",
+    },
+];
