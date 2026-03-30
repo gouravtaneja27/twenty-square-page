c556ba82302f28b4a1dbd6da88ac9677b541a12e
Gourav Taneja <gouravtaneja@Gouravs-MacBook-Air.local>
2026-03-30 10:01:54 +0530

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,10 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <div className="relative w-full max-w-324 mx-auto">
-
-      {/* Image */}
       <img
         src="/assets/bg-img.webp"
         alt="bg-img"
-        className="w-full  object-cover h-[1590px]"
-      />
-      {/* Hero Text */}
+        className="w-full  hidden lg:block object-cover h-397.5" />
       <Hero />
     </div>
   );
