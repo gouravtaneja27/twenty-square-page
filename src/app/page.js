b5406b0034730cf213d5ce209e318d7acc7dab53
Gourav Taneja <gouravtaneja@Gouravs-MacBook-Air.local>
2026-03-30 11:59:05 +0530

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,27 @@
-import Slider from "@/components/common/Slider";
 import Hero from "@/components/Hero";
 import Simple from "@/components/Simple";
+import Slider from "@/components/common/Slider";
 
 export default function Home() {
   return (
-    <div className=" w-full max-w-324 mx-auto">
-      {/* Image */}
+    <main className="w-full max-w-324 mx-auto">
       <img
         src="/assets/bg-img.webp"
-        alt="bg-img"
-        className="w-full -z-1 max-w-324 mx-auto absolute hidden lg:block object-cover h-397.5" />
-      {/* Hero Text */}
+        alt="background"
+        className="hidden lg:block absolute w-full max-w-324 mx-auto object-cover h-397.5 -z-1"
+      />
+
       <Hero />
-      <div className="flex lg:px-2 px-4 flex-col lg:flex-row w-full mt-20 sm:mt-32 lg:mt-87.25 items-center justify-center gap-10 lg:gap-7.75">
-        <div className="w-full"><Simple /></div>
-        <div className="w-full ">
+      <section className="w-full flex flex-col lg:flex-row items-center justify-center gap-10 lg:gap-7.75 mt-20 sm:mt-32 lg:mt-87.25 px-4 lg:px-2">
+        <div className="w-full">
+          <Simple />
+
+        </div>
+        <div className="w-full">
           <Slider />
         </div>
-      </div>
-    </div>
+
+      </section>
+    </main>
   );
 }
